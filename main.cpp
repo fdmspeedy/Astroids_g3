2cdf098ab82b46b1416015e8c453ffb2bad0113d
fdmspeedy <fdmspeedy@gmail.com>
2017-10-30 16:45:05 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,7 +17,12 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
-
+//Freddie Martinez
+//Aaron Gomez
+//Eric Smrkovsky
+//Hector Rios
+//Joshua Douglas
+//Scott Farrow
 #include "mainwindow.h"
 
 #include <QApplication>
